1707520a065789cd19b11534f9a0cdff553c2dc3
rani <raniyaffe@gmail.com>
2025-11-02 17:16:13 +0200

    Add: add CI

--- src/app.ts
+++ src/app.ts
@@ -18,14 +18,14 @@ const buildApp = async () => {
 	app.setValidatorCompiler(validatorCompiler);
 	app.setSerializerCompiler(serializerCompiler);
 
-	app.addHook("onReady", async () => {
-		await connectDB();
-		app.log.info("MongoDB connected");
-	});
-	app.addHook("onClose", async () => {
-		await closeDB();
-		app.log.info("MongoDB connection closed");
-	});
+	// app.addHook("onReady", async () => {
+	// 	await connectDB();
+	// 	app.log.info("MongoDB connected");
+	// });
+	// app.addHook("onClose", async () => {
+	// 	await closeDB();
+	// 	app.log.info("MongoDB connection closed");
+	// });
 
 	app.register(healthRouter, { prefix: "health" });
 	app.register(soldierRouter, { prefix: "soldiers" });
